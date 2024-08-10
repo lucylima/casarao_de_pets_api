ed1b4c624e968a7dc7df13a9d8da275c6a671171
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-08-10 00:09:21 -0300

    breaking change: prisma movido para uma nova pasta

--- src/controller/owner.controller.js
+++ src/controller/owner.controller.js
@@ -82,7 +82,7 @@ const deleteOwner = async (req, res) => {
       return res.status(404).json('404 not found')
     }
   } catch (error) {
-    res.status(400).json({ error })
+    return res.status(400).json({ error })
   }
 }
 
